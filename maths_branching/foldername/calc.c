4c0602dd4ada0872158da1168bb1876d09af1d61
Judyta Sylwia Dabek <P15238407@GWHXX680WIN003W.lec-admin.dmu.ac.uk>
2017-03-06 21:56:51 +0000

    new methods added

---
+++ maths_branching/foldername/calc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float add (float x, float y)
+{
+    return x + y;
+}
+
+
+
+float multi (float x, float y)
+{
+    return x * y;
+}
+
+float sub (float x, float y)
+{
+    return x - y;
+
+}
+}
