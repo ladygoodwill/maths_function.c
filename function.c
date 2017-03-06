7122074f73524325482fbea3356d921c500eb871
Judyta Sylwia Dabek <P15238407@GWHXX680WIN003W.lec-admin.dmu.ac.uk>
2017-03-06 20:08:44 +0000

    added division

--- function.c
+++ function.c
@@ -9,4 +9,5 @@ int b = 5;
 printf("Addition : %d + %d = %d", a, b, a+b);
 printf("Subtraction : %d - %d = %d", a, b, a-b);
 printf("Multiplication : %d * %d = %d ", a, b, a*b);
+printf("Diivision : %d / %d = %d ", a, b, a/b);
 }
